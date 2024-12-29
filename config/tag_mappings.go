1ed014c209d0662e1221046a4b49c7d36baa251a
NIXIDE <dominik.pushkarev@gmail.com>
2024-12-29 22:56:00 +0300

    refactor: move tag mapping defaults to tag_mappings.go

---
+++ config/tag_mappings.go
@@ -0,0 +1,90 @@
+package config
+
+import "fmt"
+
+func ValidateTagMappings(m map[string]map[string]string) error {
+	for format, mappings := range m {
+		if !PermittedValue(format, SupportedTagMappingFormats...) {
+			return fmt.Errorf("invalid tag mapping format '%s'", format)
+		}
+
+		for field := range mappings {
+			if !PermittedValue(field, SupportedTagMappingFields...) {
+				return fmt.Errorf("invalid tag mapping field '%s'", field)
+			}
+		}
+	}
+	return nil
+}
+
+var (
+	SupportedTagMappingFormats = []string{
+		"flac",
+		"m4a",
+	}
+
+	SupportedTagMappingFields = []string{
+		"track_id",
+		"track_url",
+		"track_name",
+		"track_artists",
+		"track_remixers",
+		"track_number",
+		"track_number_with_total",
+		"track_genre",
+		"track_subgenre",
+		"track_genre_with_subgenre",
+		"track_subgenre_or_genre",
+		"track_key",
+		"track_bpm",
+		"track_isrc",
+
+		"release_id",
+		"release_url",
+		"release_name",
+		"release_artists",
+		"release_remixers",
+		"release_date",
+		"release_year",
+		"release_track_count",
+		"release_catalog_number",
+		"release_upc",
+		"release_label",
+		"release_label_url",
+	}
+
+	DefaultTagMappings = map[string]map[string]string{
+		"flac": {
+			"track_name":              "TITLE",
+			"track_artists":           "ARTIST",
+			"track_number":            "TRACKNUMBER",
+			"track_subgenre_or_genre": "GENRE",
+			"track_key":               "KEY",
+			"track_bpm":               "BPM",
+			"track_isrc":              "ISRC",
+
+			"release_name":           "ALBUM",
+			"release_artists":        "ALBUMARTIST",
+			"release_date":           "DATE",
+			"release_track_count":    "TOTALTRACKS",
+			"release_catalog_number": "CATALOGNUMBER",
+			"release_label":          "LABEL",
+		},
+		"m4a": {
+			"track_name":    "TITLE",
+			"track_artists": "ARTIST",
+			"track_number":  "TRACKNUMBER",
+			"track_genre":   "GENRE",
+			"track_key":     "KEY",
+			"track_bpm":     "BPM",
+			"track_isrc":    "ISRC",
+
+			"release_name":           "ALBUM",
+			"release_artists":        "ALBUMARTIST",
+			"release_date":           "DATE",
+			"release_track_count":    "TOTALTRACKS",
+			"release_catalog_number": "CATALOGNUMBER",
+			"release_label":          "LABEL",
+		},
+	}
+)
